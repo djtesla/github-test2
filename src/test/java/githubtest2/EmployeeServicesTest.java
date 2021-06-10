1d725c88088c93d9a4bee65870af1b4747b6ab1d
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-06-10 14:50:44 +0200

    Fix TestCases

--- src/test/java/githubtest2/EmployeeServicesTest.java
+++ src/test/java/githubtest2/EmployeeServicesTest.java
@@ -22,11 +22,22 @@ class EmployeeServicesTest {
 
     @Test
     void testFindOldestEmployee() {
+        services.save(new Employee("John Doe", 1970, 200_000));
+        services.save(new Employee("Jane Doe", 1980, 250_000));
+        services.save(new Employee("Jack Doe", 1990, 180_000));
 
+        assertEquals("Jack Doe", services.findOldestEmployee());
     }
 
     @Test
     void testFindBySalaryLargerThan() {
+        services.save(new Employee("John Doe", 1970, 200_000));
+        services.save(new Employee("Jane Doe", 1980, 250_000));
+        services.save(new Employee("Jack Doe", 1990, 180_000));
 
+        List<Employee> result = services.findBySalaryLargerThan(200_000);
+
+        assertEquals(1, result.size());
+        assertEquals("Jane Doe", result.stream().map(Employee::getName).toList().get(0));
     }
 }
