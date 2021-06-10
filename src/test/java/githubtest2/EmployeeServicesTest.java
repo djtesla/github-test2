5249c2a0704e2313c137902b43b6ce324fa76aad
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-06-10 14:43:46 +0200

    Add TestCases to project

---
+++ src/test/java/githubtest2/EmployeeServicesTest.java
@@ -0,0 +1,32 @@
+package githubtest2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServicesTest {
+
+    private EmployeeServices services = new EmployeeServices();
+
+    @Test
+    void testSaveEmployeeThenList() {
+        services.save(new Employee("John Doe", 1970, 200_000));
+        List<Employee> employees = services.getEmployees();
+
+        assertEquals(1, employees.size());
+        assertEquals("John Doe", employees.get(0).getName());
+    }
+
+    @Test
+    void testFindOldestEmployee() {
+
+    }
+
+    @Test
+    void testFindBySalaryLargerThan() {
+
+    }
+}
--- src/test/java/githubtest2/EmployeeTest.java
+++
@@ -1,7 +0,0 @@
-package githubtest2;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class EmployeeTest {
-
-}
